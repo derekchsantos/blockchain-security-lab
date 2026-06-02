7fe4be8895a78f7caa53cb148f57fc02eae9e551
Derek Christopher Santos <48655103+derekchsantos@users.noreply.github.com>
2026-06-02 13:09:17 -0300

    Add author comment to MerkleVerifier contract

--- src/MerkleVerifier.sol
+++ src/MerkleVerifier.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+//@author Derek Christopher
+
 contract MerkleVerifier {
     // O Merkle Root é a única coisa que guardamos no contrato
     bytes32 public root;
